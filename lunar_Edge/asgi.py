862fc3a643a045498c650c8faba44a9edcd227bb
Abhijith M R <abhijithmr581@gmail.com>
2023-09-10 21:30:38 +0530

    First commit

---
+++ lunar_Edge/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for lunar_Edge project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunar_Edge.settings')
+
+application = get_asgi_application()
