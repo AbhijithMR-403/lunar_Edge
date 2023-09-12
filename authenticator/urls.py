8437dbe994a6be804021d55ea29d81521f5f28e2
Abhijith M R <abhijithmr581@gmail.com>
2023-09-12 15:28:52 +0530

    About to start authentication backend part-1 12 09

--- authenticator/urls.py
+++ authenticator/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from authenticator import views
 
+app_name = 'user_partition'
+
 urlpatterns = [
-    path('',views.userLogin,name='userlogin'),
-#     path('signup/',views.userSignup,name='usersignup'),
+    path('login/',views.user_login,name='userlogin'),
+    path('signup/',views.user_signup,name='usersignup'),
 #     path('otp/',views.otp,name='otp'),
+    path('',views.home_page,name='home')
 ]
--- authenticator/views.py
+++ authenticator/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-def userLogin(request):
-    print(request.method)
-    if request.method != 'POST':
-       return render(request,'user_partition/user_authentication/login.html')
-    email=request.POST['email']
-    password=request.POST['password']
-    return HttpResponse('hello')
+def user_login(request):
+   #  print(request.method)
+   #  if request.method != 'POST':
+   #     return render(request,'user_partition/user_authentication/login.html')
+   #  email=request.POST['email']
+   #  password=request.POST['password']
+   #  return HttpResponse('hello')
+   return render(request, 'user_partition/user_authentication/login.html')
 
+
+
+def user_signup(request):
+   return render(request,'user_partition/user_authentication/signup.html')
+
+
+
+def home_page(request):
+   return render(request,'user_partition/user_page/home.html')
